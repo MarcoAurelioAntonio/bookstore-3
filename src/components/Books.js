ccff66eb6a90d9f2556fa6bba03b882d9c82b889
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-10-03 21:00:19 -0300

    Add book's actions and reducer using the ducks pattern

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,10 +5,10 @@ import '../styles/Books.css';
 function Books() {
   return (
     <div className="book-list">
-      <Book author="Pepito" title="The lodr of los anillos" value={22} category="Action" />
-      <Book author="Juanito" title="meditacions" value={50} category="Science Fiction" />
-      <Book author="Ranita de Metan" title="enquiridion" value={60} category="Terror" />
-      <Book author="Pinocho" title="blancanieves" value={80} category="Suspenso" />
+      <Book author="Pepito" title="The lodr of los anillos" value={25} category="Action" />
+      <Book author="Juanito" title="meditacions" value={53} category="Science Fiction" />
+      <Book author="Ranita de Metan" title="enquiridion" value={69} category="Terror" />
+      <Book author="Pinocho" title="blancanieves" value={9} category="Suspenso" />
     </div>
   );
 }
