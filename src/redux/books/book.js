7ee7252600b792b770b24c5034c8843daf65fc54
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-10-07 15:45:02 -0300

    Changes to connect to the API

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,6 +1,14 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// URL API
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const URLid = '/apps/tdUGvbgxfHyC017dl4Kz/books';
+const BASE_URL = `${URL}${URLid}`;
+
 // Actions
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
+const FETCH_BOOK = 'FETCH_BOOK';
 
 // Action creators
 export const addBook = (payload) => ({ type: ADD_BOOK, payload });
@@ -17,3 +25,37 @@ export default function booksReducer(state = initialState, action) {
     default: return state;
   }
 }
+
+// API FETCH
+export const fetchBooks = createAsyncThunk(FETCH_BOOK, async () => {
+  await fetch(BASE_URL)
+    .then((res) => res.json())
+    .then((books) => {
+      const bookList = [];
+      Object.keys(books).forEach((key) => {
+        bookList.push({
+          item_id: key,
+          author: books[key][0].author,
+          title: books[key][0].title,
+          category: books[key][0].category,
+        });
+      });
+    });
+});
+
+// Add book in API
+export const addBookFetch = ({ id, title, category }) => async (dispatch) => {
+  const newBook = {
+    item_id: id,
+    title,
+    category,
+  };
+  await fetch(BASE_URL, {
+    method: 'POST',
+    body: JSON.stringify(newBook),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  dispatch(addBook({ id, title, category }));
+};
