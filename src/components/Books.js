89d2aa7343e0be56e6e16fb0522817df18d70ec8
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-10-07 13:41:08 -0300

    Update readme file, and clean commented codes

--- src/components/Books.js
+++ src/components/Books.js
@@ -10,10 +10,6 @@ function Books() {
 
       { books?.map((book) => <Book key={book.id} book={book} value={17} />)}
 
-      {/* <Book author="Pepito" title="The lodr of los anillos" value={25} category="Action" />
-      <Book author="Juanito" title="meditacions" value={53} category="Science Fiction" />
-      <Book author="Ranita de Metan" title="enquiridion" value={69} category="Terror" /> */}
-      {/* <Book author="Pinocho" title="blancanieves" value={9} category="Suspenso" /> */}
       <div className="footer-empty" />
     </div>
   );
